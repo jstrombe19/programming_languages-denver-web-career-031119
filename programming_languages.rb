47de230aca29949136447c2cc0a7f16c103c66fb
github username <github email address>
2019-03-07 16:36:32 -0700

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,22 @@
 def reformat_languages(languages)
   # your code here
+  reformatted_language = {}
+  styles = languages.keys
+  languages.each do |style, data|
+    data.each do |language, type|
+      if !reformatted_language.has_key?(language)
+        reformatted_language[language] = {
+        :type => type[:type],
+        :style => []
+      }
+        
+      end
+      styles.each do |s|
+        if s == style
+          reformatted_language[language][:style] << s
+        end
+      end
+    end
+  end
+  return reformatted_language
 end
